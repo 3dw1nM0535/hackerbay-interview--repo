15c650412506e508e84c28155c2976bdca585812
3dw1nM0535 <workockmoses@gmail.com>
2018-04-16 22:22:37 +0300

    implemented user signup

--- api/controllers/routes/signup.js
+++ api/controllers/routes/signup.js
@@ -1,1 +1,20 @@
 // Sign up route handler
+import express from "express";
+
+import User from "../../models/user";
+import parseErrors from "../../utils/parseError";
+
+const Router = express.Router();
+
+Router.post("/", (req, res) => {
+  const username = req.body.username;
+  const newUser = new User({
+    username,
+  });
+  newUser.setPassword(req.body.password);
+  newUser.save().then(() =>
+    res.json({ message: "User created successfully" }))
+    .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
+export default Router;
