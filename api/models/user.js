33ddd6a14b0b8bd36569b34635d9cd54a1ce2112
3dw1nM0535 <workockmoses@gmail.com>
2018-04-16 22:23:36 +0300

    defined user data model schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import uniqueValidator from "mongoose-unique-validator";
 
 const Schema = mongoose.Schema;
 
@@ -7,11 +8,17 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: {
     type: String,
+    required: true,
+    index: true,
+    unique: true,
   },
   passwordHash: {
     type: String,
   },
-});
+}, { versionKey: false, timestamps: true });
+
+// Enforce uniqueness to username
+userSchema.plugin(uniqueValidator, { message: "This username is already taken" });
 
 // Set password and store as hash(secure) in the database
 userSchema.methods.setPassword = function setPassword(password) {
