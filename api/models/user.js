d2defdbcb88e76e1519bd221a67a3024b1139dca
3dw1nM0535 <workockmoses@gmail.com>
2018-04-17 01:57:47 +0300

    defined user data model schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import uniqueValidator from "mongoose-unique-validator";
+import jwt from "jsonwebtoken";
+
+import configs from "../../config/configs";
 
 const Schema = mongoose.Schema;
 
@@ -17,12 +19,21 @@ const userSchema = new Schema({
   },
 }, { versionKey: false, timestamps: true });
 
-// Enforce uniqueness to username
-userSchema.plugin(uniqueValidator, { message: "This username is already taken" });
-
 // Set password and store as hash(secure) in the database
 userSchema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+// Validate password during login
+userSchema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
+// Sign user data and send as jwt-token
+userSchema.methods.generateToken = function generateToken() {
+  return jwt.sign({
+    username: this.username,
+  }, configs.SECRET_KEY);
 };
 
 export default mongoose.model("User", userSchema);
