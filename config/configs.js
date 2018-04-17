aef9ba1230b5afeadf16ebe154601ff8583dd77a
3dw1nM0535 <workockmoses@gmail.com>
2018-04-17 17:12:58 +0300

    added configs for cloudinary image management cloud

--- config/configs.js
+++ config/configs.js
@@ -8,4 +8,7 @@ export default {
   TEST_DB_URI: process.env.TEST_DB_URI,
   NODE_ENV: process.env.NODE_ENV,
   SECRET_KEY: process.env.SECRET_KEY,
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  API_KEY: process.env.API_KEY,
+  API_SECRET: process.env.API_SECRET,
 };
