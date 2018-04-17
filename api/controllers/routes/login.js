742f838778faf6302dd9894d69d24e804a2eb1c0
3dw1nM0535 <workockmoses@gmail.com>
2018-04-18 01:59:23 +0300

    return error for  undefined requests

--- api/controllers/routes/login.js
+++ api/controllers/routes/login.js
@@ -6,18 +6,22 @@ import User from "../../models/user";
 const Router = express.Router();
 
 Router.post("/", (req, res) => {
-  User.findOne({ username: req.body.username }).then((user) => {
-    if (!user) {
-      res.status(404).json({ message: "User not found. Please signup first" });
-    } else if (user) {
-      if (!user.isValidPassword(req.body.password)) {
-        res.status(401).json({ message: "Wrong password" });
+  if (req.body.username === "" || req.body.password === "") {
+    res.status(400).json({ error: "Invalid credentials" });
+  } else {
+    User.findOne({ username: req.body.username }).then((user) => {
+      if (!user) {
+        res.status(404).json({ message: "User not found. Please signup first" });
+      } else if (user) {
+        if (!user.isValidPassword(req.body.password)) {
+          res.status(401).json({ message: "Wrong password" });
+        }
+        if (user.isValidPassword(req.body.password)) {
+          res.status(200).json({ token: user.generateToken() });
+        }
       }
-      if (user.isValidPassword(req.body.password)) {
-        res.status(200).json({ token: user.generateToken() });
-      }
-    }
-  });
+    });
+  }
 });
 
 export default Router;
